1f8cde50d85c8673aabca868d5f8bb1d8d679a89
alexlnkp <leny4kap@gmail.com>
2023-07-30 16:00:56 +0700

    main commit

--- i18n.py
+++ i18n.py
@@ -4,25 +4,28 @@ import os
 
 
 def load_language_list(language):
-    with open(f"./i18n/{language}.json", "r", encoding="utf-8") as f:
-        language_list = json.load(f)
-    return language_list
+    try:
+        with open(f"./i18n/{language}.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Failed to load language file for {language}. Check if the correct .json file exists.")
+        return {}
 
 
 class I18nAuto:
     def __init__(self, language=None):
-        if language in ["Auto", None]:
-            language = locale.getdefaultlocale()[
-                0
-            ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./i18n/{language}.json"):
+        language = language or locale.getdefaultlocale()[0]
+        if not self._language_exists(language):
             language = "en_US"
-        self.language = language
-        # print("Use Language:", language)
         self.language_map = load_language_list(language)
+        self.language = language
+
+    @staticmethod
+    def _language_exists(language):
+        return os.path.exists(f"./i18n/{language}.json")
 
     def __call__(self, key):
         return self.language_map.get(key, key)
 
     def print(self):
-        print("Use Language:", self.language)
+        print(f"Using Language: {self.language}")
